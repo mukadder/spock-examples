b1cef4d9f7417b7f16c0fd0b831b642bdacce0e1
evavanya <hht179@gmail.com>
2015-12-23 13:16:59 -0500

    vcc

--- unit-testing/configuration-maven/src/test/java/net/petrikainulainen/spock/Java8StreamTest.java
+++ unit-testing/configuration-maven/src/test/java/net/petrikainulainen/spock/Java8StreamTest.java
@@ -32,6 +32,7 @@ import java.util.List;
 //import java.util.stream.Stream;
 
 import org.paumard.model.McDonald;
+import org.paumard.model.Student;
 public class Java8StreamTest {
 	
 	List<Item> items;
@@ -314,6 +315,27 @@ items = new ArrayList<>();
       System.out.println("The city has the most MacDonald : " + entry) ;
   }
 
+	@Test
 	
+public void modelgroupingby(){
+		Student s1 = new Student("Ram", "A", 20);
+        Student s2 = new Student("Shyam", "B", 22);
+        Student s3 = new Student("Mohan", "A", 22);
+        Student s4 = new Student("Mahesh", "C", 20);
+        Student s5 = new Student("Krishna", "B", 21);
+        List<Student> list = Arrays.asList(s1,s2,s3,s4,s5);	
+        System.out.println("----Group Student on the basis of ClassName----");
+        Map<String, List<Student>> stdByClass = list.stream()
+                    .collect(Collectors.groupingBy(Student::getClassName));
+        stdByClass.forEach((k,v)->System.out.println("Key:"+k+"  "+ 
+                ((List<Student>)v).stream().map(m->m.getName()).collect(Collectors.joining(","))));
+      //Group Student on the basis of age
+        System.out.println("----Group Student on the basis of age----");
+        Map<Integer, List<Student>> stdByAge = list.stream()
+                    .collect(Collectors.groupingBy(Student::getAge));
+        
+        stdByAge.forEach((k,v)->System.out.println("Key:"+k+"  "+ 
+                ((List<Student>)v).stream().map(m->m.getName()).collect(Collectors.joining(","))));
+	}
     
 }
