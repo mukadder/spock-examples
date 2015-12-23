8bee1499217cd6886a96b3cf4600ed246f278934
evavanya <hht179@gmail.com>
2015-12-23 11:52:20 -0500

    cdfsdfasdfs

--- unit-testing/configuration-maven/src/test/java/net/petrikainulainen/spock/Java8StreamTest.java
+++ unit-testing/configuration-maven/src/test/java/net/petrikainulainen/spock/Java8StreamTest.java
@@ -23,6 +23,15 @@ import java.util.Set;
 import static java.util.stream.Collectors.toSet;
 import java.util.stream.DoubleStream;
 import org.apache.log4j.Logger;
+import static com.google.common.truth.Truth.assertThat;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+//import java.util.Map;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+
+import org.paumard.model.McDonald;
 public class Java8StreamTest {
 	
 	List<Item> items;
@@ -252,7 +261,34 @@ items = new ArrayList<>();
 		}).collect(Collectors.toList());
     	
     }
-    
+  @Test
+  
+  public void testmacd() throws Exception{
+	  Stream<String> lines = Files.lines(Paths.get("files", "mcdonalds.csv")) ;
+      List<McDonald> mcdos = lines.map(s -> {
+          // -149.95038,61.13712,"McDonalds-Anchorage,AK","3828 W Dimond Blvd, Anchorage,AK, (907) 248-0597"
+          // -72.84817,41.27988,"McDonalds-Branford,CT","424 W Main St, Branford,CT, (203) 488-9353"
+          String [] strings = s.split(",") ;
+          McDonald mdo = new McDonald() ;
+          mdo.setLatitude(Double.parseDouble(strings[0])) ;
+          mdo.setLongitude(Double.parseDouble(strings[1])) ;
+          mdo.setName(strings[2].substring(1) + strings[3].substring(0, strings[3].length() - 1)) ;
+          mdo.setAddress(strings[4].substring(1)) ;
+          mdo.setCity(strings[5].trim()) ;
+          mdo.setState(strings[6].trim()) ;
+          if (mdo.state().endsWith("\"")) {
+              mdo.setState(mdo.state().substring(0, mdo.state().length() - 1)) ;
+          }
+          if (mdo.state().contains(" ")) {
+              mdo.setState(mdo.state().substring(0, mdo.state().indexOf(" "))) ;
+          }
+          if (mdo.state().length() > 2) {
+              mdo.setState(strings[7].trim()) ;
+          }
+          return mdo ;
+      }).collect(Collectors.toList()) ;
+	  assertEquals(13912,mcdos.size());
+  }
 
 	
     
